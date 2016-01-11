dd6a28c9288d62087b9e51e6739f1cb71a17c623
kenechilearnscode <kncokolo@gmail.com>
2016-01-11 16:48:32 +0000

    Complete tutorial code

--- interstitialAdTutorial2/ViewController.swift
+++ interstitialAdTutorial2/ViewController.swift
@@ -7,17 +7,60 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GADInterstitialDelegate {
+    
+    var interstitialAd: GADInterstitial?
+    
+    struct Constants {
+        static let adRate = 17
+    }
+    
+    @IBAction func showAdButton(sender: UIButton) {
+        randomPresentationOfAdWithFrequency(Constants.adRate)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        interstitialAd = createAndLoadInterstitial()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createAndLoadInterstitial() -> GADInterstitial {
+        let request = GADRequest()
+        let interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        interstitial.delegate = self
+        interstitial.loadRequest(request)
+        return interstitial
+    }
+    
+    func interstitialDidDismissScreen(ad: GADInterstitial!) {
+        interstitialAd = createAndLoadInterstitial()
+    }
+    
+    func randomNumberInRange(lower: Int, upper: Int) -> Int {
+        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
+    
+    func randomPresentationOfAdWithFrequency(oneIn: Int) {
+        let randomNumber = randomNumberInRange(1, upper: oneIn)
+        
+        print("Random number: \(randomNumber)")
+        
+        if randomNumber == 1 {
+            if interstitialAd != nil {
+                if interstitialAd!.isReady {
+                    interstitialAd?.presentFromRootViewController(self)
+                    print("Ad presented")
+                } else {
+                    print("Ad was not ready for presentation")
+                }
+            }
+        }
+        
     }
 
 
