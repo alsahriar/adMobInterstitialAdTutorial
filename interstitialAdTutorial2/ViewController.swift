c82b4c0ebd6f3014b80bc9d309a5b67d98fbb8ad
kenechilearnscode <kncokolo@gmail.com>
2016-01-11 16:24:50 +0000

    Initial Commit

---
+++ interstitialAdTutorial2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  interstitialAdTutorial2
+//
+//  Created by Kc on 11/01/2016.
+//  Copyright © 2016 Kenechi Okolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
